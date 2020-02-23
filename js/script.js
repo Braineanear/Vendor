4276b03e2a9c6c389a61a33abe158124915e2603
Mahmoud Yasser Mahmoud <mle.mahmoud.yasser@gmail.com>
2020-02-23 21:03:59 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $(function () {
+    $(document).scroll(function () {
+      var $nav = $(".fixed-top");
+      $nav.toggleClass('scrolled', $(this).scrollTop() > $nav.height());
+    });
+  });
+
+});
+    
+    
